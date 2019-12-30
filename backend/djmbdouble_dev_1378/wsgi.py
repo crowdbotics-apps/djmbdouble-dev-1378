8b016568ac4c36776b9dde15c436fc13da1f89e6
Crowdbotics <team@crowdbotics.com>
2019-12-30 19:30:13 +0000

    Initial commit.

---
+++ backend/djmbdouble_dev_1378/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for djmbdouble_dev_1378 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djmbdouble_dev_1378.settings')
+
+application = get_wsgi_application()
